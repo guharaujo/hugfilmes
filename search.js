7e5be0d59208b28deb20d7bd464ab05d60e21dab
gustavo <araujogustavo2015@gmail.com>
2020-12-03 03:13:16 -0300

    aisa

--- search.js
+++ search.js
@@ -1,19 +1,12 @@
-const TMDB_ENDPOINT_BASE = 'https://api.themoviedb.org/3';
-const TMDB_IMAGES_ENDPOINT_BASE = 'https://image.tmdb.org/t/p/w500';
+const TMDB_ENDPOINT_BASE = 'https://api.themoviedb.org/3'
+const TMDB_IMAGES_ENDPOINT_BASE = 'https://image.tmdb.org/t/p/w500'
 const API_KEY = '867304b3aa01eb018ca96fc57f0e53e5'
 
-async function RealizarPesquisa() {
+async function redirecionaPesquisa() {
     let nomeFilme = $('#search').val();
-
-    return await $.ajax({
-        url: TMDB_ENDPOINT_BASE + "/search/movie",
-        data: {
-            api_key: API_KEY,
-            query: nomeFilme
-        }
-    }).done(function(data) {
-        return data;
-    });
+    sessionStorage.setItem('busca', nomeFilme)
+    
+    window.location = "./pesquisa.html"
 }
 
 async function pegaDestaques() {
@@ -34,6 +27,7 @@ async function pegaLancamento() {
             api_key: API_KEY
         }
     }).done(function(data) {
+        console.log(data)
         return data;
     });
 }
@@ -50,10 +44,16 @@ async function pegaImagem(movie_id) {
 }
 
 $(document).ready(function() {
+    $('#btn_search').click(function(){
+        let search = redirecionaPesquisa().then(function(data) {
+            return data;
+        });
+    });
+
     pegaLancamento().then((movie) => {
         pegaImagem(movie.id).then((infoImagem) => {
             $('#imagemLancamento').append(`<a href=\"https://www.themoviedb.org/movie/${movie.id}-${movie.title}\"><img style=\"width: 150px;\" src=\"${TMDB_IMAGES_ENDPOINT_BASE + infoImagem.posters[0].file_path}\"></img></a>`)
-        }).catch((err) => $('#imagemLancamento').html('<p>Sem imagem disponivel</p>'))
+        }).catch((err) => $('#imagemLancamento').html(`<a href=\"https://www.themoviedb.org/movie/${movie.id}-${movie.title}\"><p>Sem imagem disponivel</p></a>`))
 
         $('#nomeDoLancamento').html(movie.original_title)
         $('#sinopse').html(movie.overview)
@@ -62,12 +62,4 @@ $(document).ready(function() {
     pegaDestaques().then((result) => {
         result.results.slice(0, 5).map((movie) => $('#containerDestaques').append(`<a href=\"https://www.themoviedb.org/movie/${movie.id}-${movie.title}\"><img style=\"width: 150px;\" src=\"${TMDB_IMAGES_ENDPOINT_BASE + movie.poster_path}\"></img></a>`))
     })
-
-    $("#btn_search").click(function(){
-        let data = RealizarPesquisa().then((result) => {
-            return result;
-        });
-        
-    });
-
 });
