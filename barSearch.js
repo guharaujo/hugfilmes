c317beb97865bd3cefd93bc94ba20c406b686344
gustavo <araujogustavo2015@gmail.com>
2020-12-02 23:16:56 -0300

    primeiro

---
+++ barSearch.js
@@ -0,0 +1,25 @@
+const TMDB_ENDPOINT_BASE_SEARCH = 'https://api.themoviedb.org/3';
+const API_KEY_SEARCH = '867304b3aa01eb018ca96fc57f0e53e5'
+
+async function RealizarPesquisa() {
+    let nomeFilme = $('#search').val();
+
+    return await $.ajax({
+        url: TMDB_ENDPOINT_BASE_SEARCH + "/search/movie",
+        data: {
+            api_key: API_KEY_SEARCH,
+            query: nomeFilme
+        }
+    }).done(function(data) {
+        return data;
+    });
+}
+
+$(document).ready(function() {
+    $('#btn_search').click(function(){
+        let search = RealizarPesquisa().then(function(data){
+            return data;
+        });
+        
+    });
+});
